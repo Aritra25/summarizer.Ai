e302c94afbbf029df20f387d1b31ede800082bb5
Aritra25 <aritra.maji2001@gmail.com>
2025-04-04 10:52:03 +0530

    full commit

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,4 +1,4 @@
 /* eslint-disable prettier/prettier */
 export const isDev = process.env.NODE_ENV === "development";
 
-export const ORIGIN_URL = isDev ? "http://localhost:3000" : ''
+export const ORIGIN_URL = isDev ? "http://localhost:3000" : 'https://summarizer-ji6psu4fv-aritra25s-projects.vercel.app'
